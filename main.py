ce7e1d0f829da6b26e0f09f77f9f112ed5ebf9c9
benhigh22 <benjaming.high@gmail.com>
2016-02-21 11:31:08 -0500

    Split to functions and logic separated

--- main.py
+++ main.py
@@ -9,225 +9,209 @@ def read_file():
     return [line.replace('\n', '').split(",") for line in data]
 
 
-print("You can search for receiving stats about any players from the 2015 UGA Bulldogs. \n")
-print("Categories you can search by are player name, receptions, receiving yards, touchdowns, or position.")
+def search_player_name():
+    print("")
+    player_search = input("Which player do you want to search for? "
+                          "Ex: Malcolm Mitchell can be searched using MMitchell. ")
+    cur.execute("select * from player_stats where player_name = (%s);", (player_search,))
+    player_results = cur.fetchall()
 
+    if player_results == []:
+        print("Sorry no player by that name was found.")
 
-while True:
-    search_or_add = input("Would you like to search the database or add new information to it? Enter either s or a. ")
+        while True:
+            whole_db = input("Would you like to see the entire player database? Enter y or n. ")
 
-    if search_or_add == "s":
-        search_direction = input("\n" + "To search by player name, enter 'pn'. Receptions, enter 'rec'. "
-                                        "Rec yards, enter 'ry'. Touchdowns, 'tds'. Position, 'pos'. ")
+            if whole_db == "y":
+                cur.execute("select * from player_stats")
+                show_whole_db = cur.fetchall()
 
-        if search_direction == "pn":
-            print("")
-            player_search = input("Which player do you want to search for? "
-                                  "Ex: Malcolm Mitchell can be searched using MMitchell. ")
-            cur.execute("select * from player_stats where player_name = (%s);", (player_search,))
-            player_results = cur.fetchall()
-
-            if player_results == []:
-                print("Sorry no player by that name was found.")
-
-                while True:
-                    whole_db = input("Would you like to see the entire player database? Enter y or n. ")
-
-                    if whole_db == "y":
-                        cur.execute("select * from player_stats")
-                        show_whole_db = cur.fetchall()
-
-                        for player in show_whole_db:
-                            print("\n" + "Player Name:" + "\t", "\t", "\t", player[0])
-                            print("Receptions:" + "\t", "\t", "\t", "\t", "\t", player[1])
-                            print("Receiving Yards:" + "\t", "\t", "\t", player[2])
-                            print("Touchdowns:" + "\t", "\t", "\t", "\t", "\t", player[3])
-                            print("Position:" + "\t", "\t", "\t", "\t", "\t", player[4])
-                            print("")
-                        break
-
-                    elif whole_db == "n":
-                        break
-
-                    else:
-                        print("Sorry, you must enter either y or n.")
-                        continue
-
-            else:
-
-                for player in player_results:
+                for player in show_whole_db:
                     print("\n" + "Player Name:" + "\t", "\t", "\t", player[0])
                     print("Receptions:" + "\t", "\t", "\t", "\t", "\t", player[1])
                     print("Receiving Yards:" + "\t", "\t", "\t", player[2])
                     print("Touchdowns:" + "\t", "\t", "\t", "\t", "\t", player[3])
                     print("Position:" + "\t", "\t", "\t", "\t", "\t", player[4])
                     print("")
+                break
 
-        elif search_direction == "pos":
+            elif whole_db == "n":
+                break
+
+            else:
+                print("Sorry, you must enter either y or n.")
+                continue
+
+    else:
+
+        for player in player_results:
+            print("\n" + "Player Name:" + "\t", "\t", "\t", player[0])
+            print("Receptions:" + "\t", "\t", "\t", "\t", "\t", player[1])
+            print("Receiving Yards:" + "\t", "\t", "\t", player[2])
+            print("Touchdowns:" + "\t", "\t", "\t", "\t", "\t", player[3])
+            print("Position:" + "\t", "\t", "\t", "\t", "\t", player[4])
             print("")
-            search_by_position = input("Enter either wr, te, or rb to see the players at that position. ")
-            cur.execute("select player_name from player_stats where position = (%s);", (search_by_position,))
-            pos_results = cur.fetchall()
-            print("Players who are a " + search_by_position + ":")
 
-            for position in pos_results:
-                print(position[0])
 
-        elif search_direction == "rec":
+def search_pos():
+    print("")
+    search_by_position = input("Enter either wr, te, or rb to see the players at that position. ").lower()
+    cur.execute("select player_name from player_stats where position = (%s);", (search_by_position,))
+    pos_results = cur.fetchall()
+    print("Players who are a " + search_by_position + ":")
 
-            while True:
-                print("")
-                rec_options = input("Enter either greater than '>' or less than '<' to sort players by receptions? ")
+    for position in pos_results:
+        print(position[0])
 
-                if rec_options == ">":
-                    search_by_min_receptions = input("Enter a minimum number of receptions to see the "
-                                                     "players who caught more passes than that amount. ")
-                    cur.execute("select player_name from player_stats where receptions >= (%s);",
-                                (search_by_min_receptions,))
-                    rec_ops_1 = cur.fetchall()
-                    print("Players who have more than " + search_by_min_receptions + " receptions:")
 
-                    for player in rec_ops_1:
-                        print(player[0])
-                    break
+def search_rec():
+    while True:
+        print("")
+        rec_options = input("Enter either greater than '>' or less than '<' to sort players by receptions? ")
 
-                elif rec_options == "<":
-                    search_by_max_receptions = input("Enter a maximum number of receptions to see the players who "
-                                                     "caught less passes than that amount. ")
-                    cur.execute("select player_name from player_stats where receptions <= (%s);",
-                                (search_by_max_receptions,))
-                    rec_ops_2 = cur.fetchall()
-                    print("Players who have less than " + search_by_max_receptions + " receptions:")
+        if rec_options == ">":
+            search_by_min_receptions = input("Enter a minimum number of receptions to see the "
+                                             "players who caught more passes than that amount. ")
+            cur.execute("select player_name from player_stats where receptions >= (%s);",
+                        (search_by_min_receptions,))
+            rec_ops_1 = cur.fetchall()
+            print("Players who have more than " + search_by_min_receptions + " receptions:")
 
-                    for player in rec_ops_2:
-                        print(player[0])
-                    break
-
-                else:
-                    print('Not a valid choice.')
-                    continue
-
-        elif search_direction == "ry":
-
-            while True:
-                print("")
-                rec_yard_options = input("Do you want to sort by players with greater than '>' or '<' "
-                                         "a certain number of receiving yards? ")
-
-                if rec_yard_options == ">":
-                    search_by_min_yards = input("Enter a minimum number of receiving yards to see the players "
-                                                "who had more yards than that. ")
-                    cur.execute("select player_name from player_stats where receiving_yards >= (%s);",
-                                (search_by_min_yards,))
-                    rec_yards_1 = cur.fetchall()
-                    print("Players who have more than " + search_by_min_yards + " receiving yards:")
-
-                    for player in rec_yards_1:
-                        print(player[0])
-                    break
-
-                elif rec_yard_options == "<":
-                    search_by_max_yards = input("Enter a maximum number of receiving yards to see the players "
-                                                "who had less yards than that. ")
-                    cur.execute("select player_name from player_stats where receiving_yards <= (%s);",
-                                (search_by_max_yards,))
-                    rec_yards_2 = cur.fetchall()
-                    print("Players who have less than " + search_by_max_yards + " receiving yards:")
-
-                    for player in rec_yards_2:
-                        print(player[0])
-                    break
-
-                else:
-                    print('Not a valid choice.')
-                    continue
-
-        elif search_direction == "tds":
-            while True:
-                print("")
-                td_options = input("Do you want to sort by players with greater than '>' or '<' "
-                                   "a certain number of touchdowns? ")
-
-                if td_options == ">":
-                    search_by_min_tds = input("Enter a minimum number of touchdowns to see the players "
-                                              "who scored more than that. ")
-                    cur.execute("select player_name from player_stats where touchdowns >= (%s);", (search_by_min_tds,))
-                    tds_1 = cur.fetchall()
-                    print("Players who have more than " + search_by_min_tds + " touchdowns:")
-
-                    for player in tds_1:
-                        print(player[0])
-                    break
-
-                elif td_options == "<":
-                    search_by_max_tds = input("Enter a maximum number of touchdowns to see the players "
-                                              "who scored less than that. ")
-                    cur.execute("select player_name from player_stats where touchdowns <= (%s);", (search_by_max_tds,))
-                    tds_2 = cur.fetchall()
-                    print("Players who have less than " + search_by_max_tds + " touchdowns:")
-
-                    for player in tds_2:
-                        print(player[0])
-                    break
-
-                else:
-                    print('Not a valid choice.')
-                    continue
-
-        else:
-            print("Sorry that's not a valid search option.")
-
-    elif search_or_add == "a":
-
-        while True:
-            new_player = input("To add a player to the database, enter the new player name in format "
-                               "MMitchell for Malcolm Mitchell: ")
-
-            if len(new_player) <= 50:
-                valid_new_player = new_player
-
-            else:
-                print("Sorry, player name must be less than  or equal to 50 characters. ")
-                continue
-            new_receptions = input("Enter how many receptions the player had in the 2015 season: ")
-
-            if len(new_receptions) <= 3:
-                valid_new_receptions = new_receptions
-
-            else:
-                print("Sorry, number of receptions must be less than or equal to 3 characters. ")
-                continue
-            new_rec_yards = input("Enter how many receiving yards the player had in the 2015 season: ")
-
-            if len(new_rec_yards) <= 4:
-                valid_new_rec_yards = new_rec_yards
-
-            else:
-                print("Sorry, number of receiving yards must be less than or equal to 4 characters. ")
-                continue
-            new_tds = input("Enter how many touchdowns the player had in the 2015 season: ")
-
-            if len(new_tds) <= 2:
-                valid_new_tds = new_tds
-
-            else:
-                print("Sorry, number of touchdowns must be less than or equal to 2 characters. ")
-                continue
-            new_position = input("Enter the player's position. Must be either 'wr', 'te', or 'rb': ")
-
-            if len(new_position) <= 3:
-                valid_new_pos = new_position
-
-            else:
-                print("Sorry, position must be less than or equal to 3 characters. (Wr, te, or rb. ")
-                continue
-
-            cur.execute("insert into player_stats values (%s, %s, %s, %s, %s);",
-                        (valid_new_player, valid_new_receptions, valid_new_rec_yards, valid_new_tds, valid_new_pos))
-            conn.commit()
-            print("Awesome, " + new_player + " was added to the database!")
+            for player in rec_ops_1:
+                print(player[0])
             break
 
+        elif rec_options == "<":
+            search_by_max_receptions = input("Enter a maximum number of receptions to see the players who "
+                                             "caught less passes than that amount. ")
+            cur.execute("select player_name from player_stats where receptions <= (%s);",
+                        (search_by_max_receptions,))
+            rec_ops_2 = cur.fetchall()
+            print("Players who have less than " + search_by_max_receptions + " receptions:")
 
-cur.close()
-conn.close()
+            for player in rec_ops_2:
+                print(player[0])
+            break
+
+        else:
+            print('Not a valid choice.')
+            continue
+
+
+def search_ry():
+    while True:
+        print("")
+        rec_yard_options = input("Do you want to sort by players with greater than '>' or '<' "
+                                 "a certain number of receiving yards? ")
+
+        if rec_yard_options == ">":
+            search_by_min_yards = input("Enter a minimum number of receiving yards to see the players "
+                                        "who had more yards than that. ")
+            cur.execute("select player_name from player_stats where receiving_yards >= (%s);",
+                        (search_by_min_yards,))
+            rec_yards_1 = cur.fetchall()
+            print("Players who have more than " + search_by_min_yards + " receiving yards:")
+
+            for player in rec_yards_1:
+                print(player[0])
+            break
+
+        elif rec_yard_options == "<":
+            search_by_max_yards = input("Enter a maximum number of receiving yards to see the players "
+                                        "who had less yards than that. ")
+            cur.execute("select player_name from player_stats where receiving_yards <= (%s);",
+                        (search_by_max_yards,))
+            rec_yards_2 = cur.fetchall()
+            print("Players who have less than " + search_by_max_yards + " receiving yards:")
+
+            for player in rec_yards_2:
+                print(player[0])
+            break
+
+        else:
+            print('Not a valid choice.')
+            continue
+
+
+def search_tds():
+    while True:
+        print("")
+        td_options = input("Do you want to sort by players with greater than '>' or '<' "
+                           "a certain number of touchdowns? ")
+
+        if td_options == ">":
+            search_by_min_tds = input("Enter a minimum number of touchdowns to see the players "
+                                      "who scored more than that. ")
+            cur.execute("select player_name from player_stats where touchdowns >= (%s);", (search_by_min_tds,))
+            tds_1 = cur.fetchall()
+            print("Players who have more than " + search_by_min_tds + " touchdowns:")
+
+            for player in tds_1:
+                print(player[0])
+            break
+
+        elif td_options == "<":
+            search_by_max_tds = input("Enter a maximum number of touchdowns to see the players "
+                                      "who scored less than that. ")
+            cur.execute("select player_name from player_stats where touchdowns <= (%s);", (search_by_max_tds,))
+            tds_2 = cur.fetchall()
+            print("Players who have less than " + search_by_max_tds + " touchdowns:")
+
+            for player in tds_2:
+                print(player[0])
+            break
+
+        else:
+            print('Not a valid choice.')
+            continue
+
+
+def add_func():
+    while True:
+        new_player = input("To add a player to the database, enter the new player name in format "
+                           "MMitchell for Malcolm Mitchell: ")
+
+        if len(new_player) <= 50 and new_player.isalpha():
+            valid_new_player = new_player
+
+        else:
+            print("Sorry, player name must be less than or equal to 50 alphabetical characters. ")
+            continue
+        new_receptions = input("Enter how many receptions the player had in the 2015 season: ")
+
+        if not len(new_receptions) > 3 and not new_receptions.isalpha():
+            valid_new_receptions = new_receptions
+
+        else:
+            print("Sorry, number of receptions must be less than or equal to 3 numeric characters. ")
+            continue
+        new_rec_yards = input("Enter how many receiving yards the player had in the 2015 season: ")
+
+        if not len(new_rec_yards) > 4 and not new_rec_yards.isalpha():
+            valid_new_rec_yards = new_rec_yards
+
+        else:
+            print("Sorry, number of receiving yards must be less than or equal to 4 numeric characters. ")
+            continue
+        new_tds = input("Enter how many touchdowns the player had in the 2015 season: ")
+
+        if not len(new_tds) > 2 and not new_tds.isalpha():
+            valid_new_tds = new_tds
+
+        else:
+            print("Sorry, number of touchdowns must be less than or equal to 2 numeric characters. ")
+            continue
+        new_position = input("Enter the player's position. Must be either 'wr', 'te', or 'rb': ").lower()
+
+        if new_position == "wr" or new_position == "rb" or new_position == "te":
+            valid_new_pos = new_position
+
+        else:
+            print("Sorry, position must be either wr, te, or rb. ")
+            continue
+
+        cur.execute("insert into player_stats values (%s, %s, %s, %s, %s);",
+                    (valid_new_player, valid_new_receptions, valid_new_rec_yards, valid_new_tds, valid_new_pos))
+        conn.commit()
+        print("Awesome, " + new_player + " was added to the database!")
+        break
---
+++ search_add.py
@@ -0,0 +1,39 @@
+import psycopg2
+from main import search_player_name, search_pos, search_rec, search_ry, search_tds, add_func
+conn = psycopg2.connect(user="sports_user", database="sports_stats")
+cur = conn.cursor()
+
+print("You can search for receiving stats about any players from the 2015 UGA Bulldogs. \n")
+print("Categories you can search by are player name, receptions, receiving yards, touchdowns, or position.")
+
+
+while True:
+    search_or_add = input("Would you like to search the database or add new information to it? Enter either s or a. ")
+
+    if search_or_add == "s":
+        search_direction = input("\n" + "To search by player name, enter 'pn'. Receptions, enter 'rec'. "
+                                        "Rec yards, enter 'ry'. Touchdowns, 'tds'. Position, 'pos'. ")
+        if search_direction == "pn":
+            search_player_name()
+
+        elif search_direction == "pos":
+            search_pos()
+
+        elif search_direction == "rec":
+            search_rec()
+
+        elif search_direction == "ry":
+            search_ry()
+
+        elif search_direction == "tds":
+            search_tds()
+
+        else:
+            print("Sorry that's not a valid search option.")
+
+    elif search_or_add == "a":
+        add_func()
+
+
+cur.close()
+conn.close()
